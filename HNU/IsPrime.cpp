0db742d05d8b605f210d6f4d2ac65864d682807b
JSY <JSY@REALMADRID>
2026-09-09 21:13:18 +0800

    Prime

---
+++ HNU/IsPrime.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+bool Prime(int k) {
+	for (int i = 2;i <= k / 2;i++) {
+		if (k % i == 0) return false;
+	}
+	return true;
+}
+int n;
+int i;
+int main() {
+	cin >> n;
+	i = n + 1;
+	if (Prime(n)) cout << n << " is a Prime Number" << endl;
+	else {
+		while (1) {
+			if (Prime(i)) {
+				cout << "The next Prime Number is " << i;
+				break;
+			}
+			i++;
+		}
+	}
+	return 0;
+}
